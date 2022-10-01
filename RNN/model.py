21a14273808b29170014b823617a7a2266464d35
qwes5s5 <1522419171@qq.com>
2022-10-01 17:45:44 +0800

    gru lstm rnn

---
+++ RNN/model.py
@@ -0,0 +1,117 @@
+from doctest import OutputChecker
+import math
+from sndhdr import whathdr
+from zipapp import get_interpreter
+from pyrsistent import get_in
+import torch
+from torch import nn, norm
+from torch.nn import functional as F
+from d2l import torch as d2l
+
+batch_size, num_steps = 32, 35
+train_iter, vocab = d2l.load_data_time_machine(batch_size, num_steps)
+X = torch.arange(10).reshape((2, 5))
+#初始化模型参数
+def get_params(vocab_size,num_hiddens,device):
+    num_inputs = num_outputs = vocab_size
+    def normal(shape):
+        return torch.randn(size=shape,device=device)*0.01
+    #隐藏层参数
+    W_xh = normal((num_inputs,num_hiddens))
+    W_hh = normal((num_hiddens,num_hiddens))
+    b_h = torch.zeros(num_hiddens,device=device)
+    #输出层参数
+    W_hq = normal((num_hiddens,num_outputs))
+    b_q = torch.zeros(num_outputs,device=device)
+    #附加梯度
+    params = [W_xh,W_hh,b_h,W_hq,b_q]
+    for param in params:
+        param.requires_grad_(True)
+    return params
+
+def init_rnn_state(batch_size, num_hiddens, device):
+    return (torch.zeros((batch_size, num_hiddens), device=device), )
+
+def rnn(inputs,state,params):
+    W_xh, W_hh, b_h, W_hq, b_q = params
+    H, = state
+    outputs = []
+    for X in inputs:
+        H = torch.tanh(torch.mm(X,W_xh)+torch.mm(H,W_hh)+b_h)
+        Y = torch.mm(H,W_hq)+b_q
+        outputs.append(Y)
+    return torch.cat(outputs,dim=0),(H,)
+    
+class RNNModelScratch:
+    def __init__(self,vocab_size,num_hiddens,device,get_params,init_state,forward_fn):
+        self.vocab_size, self.num_hiddens = vocab_size, num_hiddens
+        self.params = get_params(vocab_size, num_hiddens, device)
+        self.init_state, self.forward_fn = init_state, forward_fn
+    def __call__(self,X,state):
+        X = F.one_hot(X.T,self.vocab_size).type(torch.float32)
+        return self.forward_fn(X,state,self.params)
+    def begin_state(self, batch_size, device):
+        return self.init_state(batch_size, self.num_hiddens, device)
+
+num_hiddens = 512
+net = RNNModelScratch(len(vocab), num_hiddens, d2l.try_gpu(), get_params,
+                      init_rnn_state, rnn)
+state = net.begin_state(X.shape[0], d2l.try_gpu())
+Y, new_state = net(X.to(d2l.try_gpu()), state)
+
+def predict_ch8(prefix,num_preds,net,vocab,device):
+    state = net.begin_state(batch_size=1,device=device)
+    outputs = [vocab[prefix[0]]]
+    get_input = lambda: torch.tensor([outputs[-1]],device=device).reshape(1,1)
+    for y in prefix[1:]:
+        _,state = net(get_input(),state)
+        outputs.append(vocab[y])
+    for _ in range(num_preds):
+        y,state = net(get_input(),state)
+        outputs.append(int(y.argmax(dim=1).reshape(1)))
+    return ' '.join([vocab.idx_to_token[i] for i in outputs])
+
+def grad_clipping(net, theta):  #@save
+    """裁剪梯度"""
+    if isinstance(net, nn.Module):
+        params = [p for p in net.parameters() if p.requires_grad]
+    else:
+        params = net.params
+    norm = torch.sqrt(sum(torch.sum((p.grad ** 2)) for p in params))
+    if norm > theta:
+        for param in params:
+            param.grad[:] *= theta / norm
+
+#@save
+def train_epoch_ch8(net, train_iter, loss, updater, device, use_random_iter):
+    """训练网络一个迭代周期（定义见第8章）"""
+    state, timer = None, d2l.Timer()
+    metric = d2l.Accumulator(2)  # 训练损失之和,词元数量
+    for X, Y in train_iter:
+        if state is None or use_random_iter:
+            # 在第一次迭代或使用随机抽样时初始化state
+            state = net.begin_state(batch_size=X.shape[0], device=device)
+        else:#前一个批次最后一个隐藏状态成为这个批次第一个隐藏状态，如果不detach掉成为叶子结点，则会反向传播出错，因为上一个批次的计算图已经删除
+            if isinstance(net, nn.Module) and not isinstance(state, tuple):
+                # state对于nn.GRU是个张量
+                state.detach_()
+            else:
+                # state对于nn.LSTM或对于我们从零开始实现的模型是个张量
+                for s in state:
+                    s.detach_()
+        y = Y.T.reshape(-1)
+        X, y = X.to(device), y.to(device)
+        y_hat, state = net(X, state)
+        l = loss(y_hat, y.long()).mean()
+        if isinstance(updater, torch.optim.Optimizer):
+            updater.zero_grad()
+            l.backward()
+            grad_clipping(net, 1)
+            updater.step()
+        else:
+            l.backward()
+            grad_clipping(net, 1)
+            # 因为已经调用了mean函数
+            updater(batch_size=1)
+        metric.add(l * y.numel(), y.numel())
+    return math.exp(metric[0] / metric[1]), metric[1] / timer.stop()
